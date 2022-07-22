683c6f548fda4368c20c86d7a9cede3a70c2252b
liuniansuiyi <97726273+liuniansuiyi@users.noreply.github.com>
2022-07-22 12:35:54 +0800

    Add files via upload
    the birthday attack of SM3

---
+++ birthday_attack/birthday_attack.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 18 12:33:06 2022
+由于时间因素，我们对SM3的生日攻击只比较前8位，
+但即使如此，在产生最终结果中也将会导致大量的时间开销
+效率不是很高效。
+"""
+
+import sys
+from gmssl.func import rotl, bytes_to_list
+from gmssl import sm3, func
+import random
+
+IV = [
+    1937774191, 1226093241, 388252375, 3666478592,
+    2842636476, 372324522, 3817729613, 2969243214,
+]
+
+T_j = [
+    2043430169, 2043430169, 2043430169, 2043430169, 2043430169, 2043430169,
+    2043430169, 2043430169, 2043430169, 2043430169, 2043430169, 2043430169,
+    2043430169, 2043430169, 2043430169, 2043430169, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042
+]
+
+
+
+def str_2_byte(msg):  # 字符串转换成byte数组
+    ml = len(msg)
+    msg_byte = []
+    msg_bytearray = msg.encode('utf-8')
+    for i in range(ml):
+        msg_byte.append(msg_bytearray[i])
+    return msg_byte
+
+
+def byte_2_str(msg):  # byte数组转字符串
+    ml = len(msg)
+    str1 = b""
+    for i in range(ml):
+        str1 += b'%c' % msg[i]
+    return str1.decode('utf-8')
+
+
+def hex_2_byte(msg):  # 16进制字符串转换成byte数组
+    ml = len(msg)
+    if ml % 2 != 0:
+        msg = '0' + msg
+    ml = int(len(msg) / 2)
+    msg_byte = []
+    for i in range(ml):
+        msg_byte.append(int(msg[i * 2:i * 2 + 2], 16))
+    return msg_byte
+
+
+def byte_2_hex(msg):  # byte数组转换成16进制字符串
+    ml = len(msg)
+    hexstr = ""
+    for i in range(ml):
+        hexstr = hexstr + ('%02x' % msg[i])
+    return hexstr
+
+
+
+def sm3_ff_j(x, y, z, j):
+    if 0 <= j and j < 16:
+        ret = x ^ y ^ z
+    elif 16 <= j and j < 64:
+        ret = (x & y) | (x & z) | (y & z)
+    return ret
+
+def sm3_gg_j(x, y, z, j):
+    if 0 <= j and j < 16:
+        ret = x ^ y ^ z
+    elif 16 <= j and j < 64:
+        #ret = (X | Y) & ((2 ** 32 - 1 - X) | Z)
+        ret = (x & y) | ((~ x) & z)
+    return ret
+
+def sm3_p_0(x):
+    return x ^ (rotl(x, 9 % 32)) ^ (rotl(x, 17 % 32))
+
+def sm3_p_1(x):
+    return x ^ (rotl(x, 15 % 32)) ^ (rotl(x, 23 % 32))
+
+def sm3_cf(v_i, b_i):
+    w = []
+    for i in range(16):
+        weight = 0x1000000
+        data = 0
+        for k in range(i*4,(i+1)*4):
+            data = data + b_i[k]*weight
+            weight = int(weight/0x100)
+        w.append(data)
+    
+    for j in range(16, 68):
+        w.append(0)
+        w[j] = sm3_p_1(w[j-16] ^ w[j-9] ^ (rotl(w[j-3], 15 % 32))) ^ (rotl(w[j-13], 7 % 32)) ^ w[j-6]
+        str1 = "%08x" % w[j]
+    w_1 = []
+    for j in range(0, 64):
+        w_1.append(0)
+        w_1[j] = w[j] ^ w[j+4]
+        str1 = "%08x" % w_1[j]
+
+    a, b, c, d, e, f, g, h = v_i
+
+    for j in range(0, 64):
+        ss_1 = rotl(
+            ((rotl(a, 12 % 32)) +
+            e +
+            (rotl(T_j[j], j % 32))) & 0xffffffff, 7 % 32
+        )
+        ss_2 = ss_1 ^ (rotl(a, 12 % 32))
+        tt_1 = (sm3_ff_j(a, b, c, j) + d + ss_2 + w_1[j]) & 0xffffffff
+        tt_2 = (sm3_gg_j(e, f, g, j) + h + ss_1 + w[j]) & 0xffffffff
+        d = c
+        c = rotl(b, 9 % 32)
+        b = a
+        a = tt_1
+        h = g
+        g = rotl(f, 19 % 32)
+        f = e
+        e = sm3_p_0(tt_2)
+
+        a, b, c, d, e, f, g, h = map(
+            lambda x:x & 0xFFFFFFFF ,[a, b, c, d, e, f, g, h])
+
+    v_j = [a, b, c, d, e, f, g, h]
+    return [v_j[i] ^ v_i[i] for i in range(8)]
+
+
+
+
+def sm3_hash(msg):
+    # print(msg)
+    len1 = len(msg)
+    reserve1 = len1 % 64
+    msg.append(0x80)
+    reserve1 = reserve1 + 1
+    # 56-64, add 64 byte
+    range_end = 56
+    if reserve1 > range_end:
+        range_end = range_end + 64
+
+    for i in range(reserve1, range_end):
+        msg.append(0x00)
+
+    bit_length = (len1) * 8
+    bit_length_str = [bit_length % 0x100]
+    for i in range(7):
+        bit_length = int(bit_length / 0x100)
+        bit_length_str.append(bit_length % 0x100)
+    for i in range(8):
+        msg.append(bit_length_str[7-i])
+
+    group_count = round(len(msg) / 64)
+
+    B = []
+    for i in range(0, group_count):
+        B.append(msg[i*64:(i+1)*64])
+
+    V = []
+    V.append(IV)
+    for i in range(0, group_count):
+        V.append(sm3_cf(V[i], B[i]))
+
+    y = V[i+1]
+    result = ""
+    for i in y:
+        result = '%s%08x' % (result, i)
+    return result
+
+
+
+def SM3(msg):
+    msg_byte = func.bytes_to_list(bytes(msg, encoding='utf-8'))
+    return sm3_hash(msg_byte)
+
+for j in range(0,1000):
+    count = 0
+    for i in range(0,2**16):
+        x = str(random.random())
+        y = str(random.random())
+        x1 = SM3(x)
+        y1 = SM3(y)
+        x2 = x1[0:8]
+        y2 = y1[0:8]
+        if(x2 == y2):
+            print("True")
+print("end")
+
